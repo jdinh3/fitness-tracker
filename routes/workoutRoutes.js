37f55a2cbdcdbab33f8f5a9a9869e6293c3fe5f6
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-05-07 14:14:31 -0500

    finished get route for stats page and updated get route for homepage

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -2,7 +2,9 @@ const Workout = require("../models/workouts");
 const router = require("express").Router();
 
 router.get("/api/workouts", (req, res) => {
-  Workout.find({})
+  Workout.aggregate([
+    { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+  ])
     .then((result) => {
       console.log(result);
       res.json(result);
@@ -34,6 +36,19 @@ router.post("/api/workouts", (req, res) => {
     });
 });
 
-router.get("/api/workouts/range", () => {});
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+  ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((result) => {
+      console.log(result);
+      res.json(result);
+    })
+    .catch((err) => {
+      if (err) throw err;
+    });
+});
 
 module.exports = router;
