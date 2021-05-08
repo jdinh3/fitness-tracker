46a2de1c022d6c5803b5f8c2eca00eb22eb424ea
Jenny Dinh <jenny.p.dinh@gmail.com>
2021-05-07 19:53:01 -0500

    small change

--- server.js
+++ server.js
@@ -10,21 +10,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
-app.use(require("./controllers/routes/view"));
 app.use(require("./controllers/routes/workoutRoutes"));
+app.use(require("./controllers/routes/view"));
 
 app.listen(PORT, () => {
   console.log(`Now listening on port ${PORT}`);
 });
-
-
